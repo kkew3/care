5c5e45cc3797e5355dd0c045df5109a85540c415
kkew3 <kps6326@hotmail.com>
2021-12-20 18:04:07 +0800

    Removed unnecessary lines

--- make_bin.sh
+++ make_bin.sh
@@ -3,9 +3,7 @@ set -e
 
 if [ -n "$VIRTUAL_ENV" ] && python3 -c "import libarchive.public" 2> /dev/null; then
     mkdir -p bin
-    cp care.py bin/tmp
-    { printf "#!%s\n" "$(command -v python3)"; cat bin/tmp; } > bin/care
-    rm bin/tmp
+    { printf "#!%s\n" "$(command -v python3)"; cat care.py } > bin/care
     chmod u+x bin/care
 else
 	echo "Must be in virtualenv containing libarchive" >&2
